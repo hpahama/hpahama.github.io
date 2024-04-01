2eaadfc385a5d0bb2b3a9520e7998bc872ec3cc0
hpahama <143666716+hpahama@users.noreply.github.com>
2024-04-01 14:37:11 -0700

    plot.js

---
+++ plot.js
@@ -0,0 +1,22 @@
+//Draft
+//Create an HTML file, plot.html
+//Create a .js file, plot.js
+<!DOCTYPE html>
+//Create a canvas element in the HTML file
+  <head>
+    <title>Create a Canvas Element In-Class Activity</title>
+    <script>
+      function f(x) {
+        //Create a heart looking math function, of which the half and half are both symmetrical to both sides
+        //The formula is 5cos(2t) - 2cos(3t) - cos(4t)
+        //There is a label on top of it that says, click the equation to reveal the output/heart
+        //heart line must be red
+        //The symetrical heart line appears upon user interaction
+      }
+      </script>
+  </head>
+//Create a listener or listeners to do the following
+  //'wasd' to scroll
+  //'p' and 'm' (plus and minus) to zoom
+  //'r', 'h', 'd' to reset defaults
+</html>
